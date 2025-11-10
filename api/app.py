5231f0d5e9aeb45cce1a7b64b0ea05dc886507ae
Kartik <gkhatri211@gmail.com>
2025-11-10 15:18:47 +0530

    Improvements4.1

--- api/app.py
+++ api/app.py
@@ -3,9 +3,7 @@ import requests
 import os
 import base64
 import re
-from dotenv import load_dotenv
 # Load environment variables from .env file
-load_dotenv()
 
 app = Flask(__name__)
 handler = app
